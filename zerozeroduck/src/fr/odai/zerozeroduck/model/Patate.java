5aedcb2b4fc97c4316c666f62da03f2b566434bd
Dorvaryn <benjamin@odai.homelinux.net>
2013-02-16 15:06:24 +0100

    Patate with stateTime

--- zerozeroduck/src/fr/odai/zerozeroduck/model/Patate.java
+++ zerozeroduck/src/fr/odai/zerozeroduck/model/Patate.java
@@ -9,12 +9,14 @@ public class Patate {
 	public enum State {
 		IDLE, WALKING, JUMPING, DYING
 	}
-
+	
 	static final float SPEED = 2f;	// unit per second
 	static final float JUMP_VELOCITY = 1f;
 	public static final float SIZE = 0.5f; // half a unit
 
+	float       stateTime = 0;
 	Vector2 	position = new Vector2();
+	Vector2     velocity = new Vector2();
 	Rectangle 	bounds = new Rectangle();
 	State		state = State.WALKING;
 	boolean		facingLeft = true;
@@ -35,5 +37,10 @@ public class Patate {
 
 	public void walkForward(float delta) {
 		this.position.x += delta * SPEED;
+	}
+	
+	public void update(float delta) {
+		stateTime += delta;
+		position.add(velocity.tmp().mul(delta)); 
 	}
 }
