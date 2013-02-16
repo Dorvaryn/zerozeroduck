f8752be4c2fa79170d39642665e8f6a10b7bce15
Dorvaryn <benjamin@odai.homelinux.net>
2013-02-16 12:40:37 +0100

    Actors init

---
+++ zerozeroduck/src/Duck.java
@@ -0,0 +1,27 @@
+
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class Duck {
+
+	public enum State {
+		IDLE, WALKING, JUMPING, DYING
+	}
+
+	static final float SPEED = 2f;	// unit per second
+	static final float JUMP_VELOCITY = 1f;
+	static final float SIZE = 0.5f; // half a unit
+
+	Vector2 	position = new Vector2();
+	Rectangle 	bounds = new Rectangle();
+	State		state = State.IDLE;
+	boolean		facingLeft = true;
+
+	public Duck(Vector2 position) {
+		this.position = position;
+		this.bounds.height = SIZE;
+		this.bounds.width = SIZE;
+	}
+	
+}
